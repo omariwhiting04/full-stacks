824d8229e04833a66d86989b5af17b66ad0b3c3c
omariwhiting04 <omariwhiting04@gmail.com>
2025-05-02 16:47:16 -0400

    Add partner 4’s industry-tagging & filter work

---
+++ Downloads/stock_favorites_clean/frontend/src/App.js
@@ -0,0 +1,13 @@
+// src/App.js
+import React from 'react';
+import FavoritesList from './FavoritesList';
+
+function App() {
+  return (
+    <div className="App">
+      <FavoritesList />
+    </div>
+  );
+}
+
+export default App;
